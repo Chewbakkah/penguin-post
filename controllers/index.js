b2724df7f8350037ce529e141d455696beb44452
Kyro <mariogallardo1331@yahoo.com>
2022-02-16 22:21:33 -0600

    fixing error not giving us tables

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,9 @@ const router = require('express').Router();
 
 const commentRoutes = require('./api/comment-routes');
 
-const postRoutes = require('./api/post-routes');
+// const postRoutes = require('./api/post-routes');
 
-// const userRoutes = require('./api/user-routes')
+// const userRoutes = require('./api/user-routes');
 
 router.use('/', commentRoutes);
 
