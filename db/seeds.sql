a72b8e085a02c869a910a98eb7ad9aced4c2e332
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-02-18 13:17:06 -0600

    add user seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,2 @@
-INSERT INTO 
+INSERT INTO user(username, email, password)
+VALUES ('hamilton', 'hamilton@mail.com', 'pass'), ('mario', 'mario@mail.com', 'pass'), ('cory', 'cory@mail.com', 'pass'), ('cheyne', 'cheyne@mail.com', 'pass');
