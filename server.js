d4816b8677d8dfe4d09588f22dc1d31f6307d65b
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-02-18 14:51:44 -0600

    refactor

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 // turn on routes
-app.use(config);
+app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
