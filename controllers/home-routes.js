d5402f19a912230b9b2bf659ba59e25930288dbd
cdtest <corydavis1989@gmail.com>
2022-02-18 13:18:02 -0600

    db fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,7 +8,7 @@ router.get('/', (req, res) => {
         attributes: [
             'id',
             'title',
-            // 'post_url',
+            'post_url',
             'user_id',
             'created_at'
         ],
@@ -41,53 +41,53 @@ router.get('/', (req, res) => {
     });
 });
 
-// Creating route to GET single post
-// Question on line 80
-// router.get('/post/:id', (req, res) => {
-//     Post.findOne({
-//         where: {
-//             id: req.params.id
-//         },
-//         attributes: [
-//             'id',
-//             // 'post_url'
-//             'title',
-//             'user_id',
-//             'created_at'
-//         ],
-//         include: [
-//             {
-//                 model: Comment,
-//                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//                 include: {
-//                     model: User, 
-//                     attributes: ['username']
-//                 }
-//             }, 
-//             {
-//                 model: User, 
-//                 attributes: ['username']
-//             }
-//         ]
-//     })
-//     .then(dbPostData => {
-//         if (!dbPostData) {
-//             // send user to Court's 404 page
-//             res.status(404).json({ message: 'No post found with this id!' });
-//             return;
-//         }
+Creating route to GET single post
+Question on line 80
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url'
+            'title',
+            'user_id',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User, 
+                    attributes: ['username']
+                }
+            }, 
+            {
+                model: User, 
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            // send user to Court's 404 page
+            res.status(404).json({ message: 'No post found with this id!' });
+            return;
+        }
 
-//         const post = dbPostData.get({ plain: true });
-//         res.render('post-info', {
-//             post,
-//             loggedIn: req.session.loggedIn
-//         });
-//     })
-//     .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//     });
-// });
+        const post = dbPostData.get({ plain: true });
+        res.render('post-info', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
