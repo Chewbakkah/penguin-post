f0f40ac616a87c6ddccfc1946a940861c40b3614
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-02-18 12:27:57 -0600

    add api routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,45 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models/');
 
-// Retrieving all post for homepage
 router.get('/', (req, res) => {
+    console.log('======================');
     Post.findAll({
-        attributes: [
-            'id',
-            
-        ]
+      attributes: [
+        'id',
+        'post_content',
+        'likes',
+        'created_at',
+        'resquawk'
+      ],
+    //   include: [
+    //     {
+    //       model: Comment,
+    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+    //       include: {
+    //         model: User,
+    //         attributes: ['username']
+    //       }
+    //     },
+    //     {
+    //       model: User,
+    //       attributes: ['username']
+    //     }
+    //   ]
     })
-})
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        if(req.session.loggedIn){
+            res.render('dashboard', {
+                posts,
+                loggedIn: req.session.loggedIn
+              });
+        }else{
+            res.render('login', {});
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
