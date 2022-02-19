eaf3e4f2f29899f7842d7831be09adaabbf43aed
Chewbakkah <chewbakkah@gmail.com>
2022-02-19 15:36:04 -0600

    pushing for group testing of image problem

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,41 +4,48 @@ const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     console.log('======================');
-    Post.findAll({
-        attributes: [
-            'id',
-            'title',
-            'post_content',
-            'user_id',
-            'created_at'
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                    model: User,
-                    attributes: ['username']
-                }
-            },
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    .then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
+if(req.session.loggedIn){
+    res.render('homepage', {
+                    posts, 
+                    loggedIn: req.session.loggedIn})}
+                    else{
+                        res.render('login');
+                    }
+//     Post.findAll({
+//         attributes: [
+//             'id',
+//             'title',
+//             'post_content',
+//             'user_id',
+//             'created_at'
+//         ],
+//         include: [
+//             {
+//                 model: Comment,
+//                 attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+//                 include: {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             },
+//             {
+//                 model: User,
+//                 attributes: ['username']
+//             }
+//         ]
+//     })
+//     .then(dbPostData => {
+//         const posts = dbPostData.map(post => post.get({ plain: true }));
         
-        res.render('login', {
-            posts, 
-            loggedIn: req.session.loggedIn
-        });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+//         res.render('login', {
+//             posts, 
+//             loggedIn: req.session.loggedIn
+//         });
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json(err);
+//     });
 });
 
 // Creating route to GET single post
