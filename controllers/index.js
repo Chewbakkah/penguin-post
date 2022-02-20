d5377e2963fd09eb8de0fb5371aacfc4b19c1bf4
court65 <court_re@yahoo.com>
2022-02-20 15:23:34 -0600

    404 page changes

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,10 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes');
-const errorRoute=require("../views/404");
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
-//All other home routes go above this one
-router.use('/*',errorRoute)
 
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
 INSERT INTO user(username, email, password)
 VALUES ('hamilton', 'hamilton@mail.com', 'pass'), ('mario', 'mario@mail.com', 'pass'), ('cory', 'cory@mail.com', 'pass'), ('cheyne', 'cheyne@mail.com', 'pass');
 
--- INSERT INTO post (title, post_content, user_id)
--- VALUES ("First Post!", "first post content", 1),
---     ("Second Post!", "second post content", 2),
---     ("Third Post!", "third post content", 3),
---     ("Fourth Post!", "fourth post content", 4),
---     ("Fifth Post!", "fifth post content", 5);
+INSERT INTO post (title, post_content, user_id)
+VALUES ("First Post!", "first post content", 1),
+    ("Second Post!", "second post content", 2),
+    ("Third Post!", "third post content", 3),
+    ("Fourth Post!", "fourth post content", 4),
+    ("Fifth Post!", "fifth post content", 5);
 
 
