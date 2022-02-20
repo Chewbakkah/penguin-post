6325398e78c93bee3740cd1677604e5879a4fc55
Chewbakkah <chewbakkah@gmail.com>
2022-02-20 17:33:12 -0600

    trying to make profile work

---
+++ controllers/profile-routes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+async function getUserPosts(id) {
+    console.log('getUserPosts called');
+    router.get('/', (req, res) => {
+        console.log('router passed');
+        console.log(id);
+      Post.findAll({
+        where: {
+          id: user_id
+        },
+        attributes: ['id', 'post_content', 'created_at'],
+        order: [['created_at', 'DESC']], 
+        include: [
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ]
+      })
+          .then(dbPostData => res.json(dbPostData))
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });
+    }
+    
+
+router.get('/:id', withAuth, (req, res) => {
+    console.log('profile id called');
+    User.findOne({
+        where: {
+            id: req.params.id
+          },
+      attributes: [
+          'id',
+        'username',
+        'icon',
+        'hero'
+      ],
+    //   include: [
+    //     {
+    //       model: Post,
+    //       attributes: ['id', 'post_content', 'created_at']
+    //     },
+    //   ]
+    })
+      .then(dbUserData => {
+        //   console.log(dbUserData.dataValues.id);
+          getUserPosts(dbUserData.dataValues.id);
+      })
+      .then(dbPostData => {
+            // console.log(dbPostData);
+          const userInfo = dbPostData;
+        //   console.log(userInfo);
+          res.render('profile', { userInfo, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+
+module.exports = router;
