42ae17b5c941a051b7da7f67c9e15e4125459b7c
Kyro <mariogallardo1331@yahoo.com>
2022-02-20 18:12:31 -0600

    creating protection on api/user findall

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,14 +3,18 @@ const { User, Post, Comment, Favorite } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
-  User.findAll({
-    attributes: { exclude: ['password'] }
-  })
+  if (req.session.loggedIn) {
+    User.findAll({
+      attributes: { exclude: ['password'] }
+    })
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+  } else {
+    res.render('login')
+  }
 });
 
 router.get('/:id', (req, res) => {
@@ -28,6 +32,7 @@ router.get('/:id', (req, res) => {
   })
     .then(dbUserData => {
       if (!dbUserData) {
+        // res.render('404')
         res.status(404).json({ message: 'No user found with this id' });
         return;
       }
@@ -40,7 +45,6 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
     email: req.body.email,
@@ -51,7 +55,6 @@ router.post('/', (req, res) => {
         req.session.user_id = dbUserData.id;
         req.session.username = dbUserData.username;
         req.session.loggedIn = true;
-  
         res.json(dbUserData);
       });
     })
@@ -62,7 +65,6 @@ router.post('/', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email
@@ -102,8 +104,6 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
     individualHooks: true,
