18b9fa2a05cd657f8b2a0bbf19375b5110cefbd7
Chewbakkah <chewbakkah@gmail.com>
2022-02-22 01:03:21 -0600

    saving for night

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -3,43 +3,6 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// router.get('/:id', withAuth, (req, res) => {
-//     console.log('profile id called');
-//     User.findOne({
-//         where: {
-//             id: req.params.id
-//           },
-//       attributes: [
-//           'id',
-//         'username',
-//         'icon',
-//         'hero'
-//       ],
-//       include: [
-//         {
-//           model: Post,
-//           attributes: ['id', 'post_content', 'user_id', 'created_at'],
-//           order: sequelize.literal('created_at DESC'),
-//           limit: 25,
-//         }
-//       ]
-      
-//     })
-
-//       .then(dbPostData => {
-//             // console.log(dbPostData);
-//           const user = dbPostData.dataValues;
-//           const posts = dbPostData.dataValues.posts.map(post => post.get({ plain: true }));
-//           console.log(user);
-//           console.log(posts);
-//           res.render('profile', { user, posts, loggedIn: true });
-//       })
-//       .catch(err => {
-//         console.log(err);
-//         res.status(500).json(err);
-//       });
-//   });
-
 router.get('/:id', withAuth, (req, res) => {
   Post.findAll({
     where: {
