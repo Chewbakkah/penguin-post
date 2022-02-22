180f9daed1a764f75a2972b187cf782877064201
Chewbakkah <chewbakkah@gmail.com>
2022-02-21 20:57:41 -0600

    profile pulling posts

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -3,34 +3,6 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-async function getUserPosts(id) {
-    console.log('getUserPosts called');
-    //getting stuck here!!!!!!!!!!!!!
-    router.get('/', (req, res) => {
-        console.log('router passed');
-        console.log(id);
-      Post.findAll({
-        where: {
-          id: user_id
-        },
-        attributes: ['id', 'post_content', 'created_at'],
-        order: [['created_at', 'DESC']], 
-        include: [
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
-          .then(dbPostData => res.json(dbPostData))
-          .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-          });
-      });
-    }
-    
-
 router.get('/:id', withAuth, (req, res) => {
     console.log('profile id called');
     User.findOne({
@@ -43,22 +15,24 @@ router.get('/:id', withAuth, (req, res) => {
         'icon',
         'hero'
       ],
-    //   include: [
-    //     {
-    //       model: Post,
-    //       attributes: ['id', 'post_content', 'created_at']
-    //     },
-    //   ]
+      include: [
+        {
+          model: Post,
+          attributes: ['id', 'post_content', 'created_at'],
+          order: sequelize.literal('created_at DESC'),
+          limit: 25,
+        },
+      ]
+      
     })
-      .then(dbUserData => {
-        //   console.log(dbUserData.dataValues.id);
-          getUserPosts(dbUserData.dataValues.id);
-      })
+
       .then(dbPostData => {
             // console.log(dbPostData);
-          const userInfo = dbPostData;
-        //   console.log(userInfo);
-          res.render('profile', { userInfo, loggedIn: true });
+          const userInfo = dbPostData.dataValues;
+          const posts = dbPostData.dataValues.posts.map(post => post.get({ plain: true }));
+          console.log(userInfo);
+          console.log(posts);
+          res.render('profile', { userInfo, posts, loggedIn: true });
       })
       .catch(err => {
         console.log(err);
