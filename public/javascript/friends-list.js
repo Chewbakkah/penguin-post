8e1b46d8397f193b3b6a94f3cff0b2ff68e0d9c1
cdtest <corydavis1989@gmail.com>
2022-02-22 22:44:11 -0600

    filling out friends-list

--- public/javascript/friends-list.js
+++ public/javascript/friends-list.js
@@ -1,10 +1,14 @@
-
 async function showFriends() {
-    const response = await fetch(`/api/users/${id}/friends`, {
-      method: 'get',
-    });
+    const response = await fetch(`/api/users/${user_id}`, {
+        method: "GET",
+        body: JSON.stringify({
+          friends,
+        }),
+        headers: { "Content-Type": "application/json" },
+      });
   
     if (response.ok) {
+        console.log(response)
     } else {
       alert(response.statusText);
     }
