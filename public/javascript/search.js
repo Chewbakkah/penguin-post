71e501946af8cc1db5964f429994123b694ff96a
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2022-02-23 00:22:16 -0600

    add url replace

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,11 +1,11 @@
 async function newSearchHandler(event) {
-    event.preventDefault();
+
   
     const search_result = document.querySelector('input[name="search-result"]').value;
     console.log(search_result);
     
     if (search_result.ok) {
-        var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.profile + window.location.search_result;
+        var newURL = window.location.protocol + "//" + window.location.host + "/" + "profile" + "/" + window.location.search_result;
         document.location.replace(newURL);
       //document.location.replace('/profile/'+ {searchValue});
     } else {
