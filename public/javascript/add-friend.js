87557f9c0a7bc6fa72612c9d1a9110c8e563f4ba
cdtest <corydavis1989@gmail.com>
2022-02-24 13:27:39 -0600

    added FE add-friend.js

--- public/javascript/add-friend.js
+++ public/javascript/add-friend.js
@@ -1,14 +1,30 @@
-async function addFriend() {    
-    let route = `/api/users/1`;
-  
-    fetch(route)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        throw err;
-      });
-  }
-  
-  document.querySelector(".add-friend").addEventListener("click", addFriend);
+// let session = require('express-session');
+
+async function addFriend(event) {
+  event.preventDefault();
+  const newFriend = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const ownerId = session.user_id;
+console.log(ownerId);
+
+  console.log(user_id);
+const response = await fetch(`/api/users/${ownerId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        friends: newFriend
+    }),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+});
+if(response.ok){
+    document.location.replace('/dashboard/');
+}else{
+    alert(response.statusText)
+}
+}
+
+
+document.querySelector(".add-friend").addEventListener("click", addFriend);
