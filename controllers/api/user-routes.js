1a2cb75e88b6503f5764a5876073640eb19e66b1
Chewbakkah <chewbakkah@gmail.com>
2022-02-24 15:55:52 -0600

    fixing profile image

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User, Post } = require("../../models");
+const { User, Post } = require("../../models/rename");
 
 // get all users
 router.get("/", (req, res) => {
