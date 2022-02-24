372ea9c58d161577af5d42aa9e52a4bb8ff00a14
Chewbakkah <chewbakkah@gmail.com>
2022-02-24 17:48:26 -0600

    pushing for presentation

--- models/User.js
+++ models/User.js
@@ -60,6 +60,7 @@ User.init(
       // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        console.log(newUserData);
         return newUserData;
       },
 
